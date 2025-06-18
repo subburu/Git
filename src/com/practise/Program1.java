c81f9128ef32343b1e724d76ecfc6dd621ebcd87
subburusravan <subburusravan@gmail.com>
2025-06-18 16:16:14 +0530

    updated program1

--- src/com/practise/Program1.java
+++ src/com/practise/Program1.java
@@ -4,9 +4,9 @@ public class Program1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Program4");
-		System.out.println("Program5");
-		System.out.println("Program6");
+		System.out.println("Program7");
+		System.out.println("Program8");
+		System.out.println("Program9");
 
 	}
 
