66f1787226d99d27dd7da0529e53b1c4c567f7f5
subburusravan <subburusravan@gmail.com>
2025-06-18 15:11:34 +0530

    new file santosh added

---
+++ src/com/practise/Santosh.java
@@ -0,0 +1,11 @@
+package com.practise;
+
+public class Santosh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Santosh Santosh");
+
+	}
+
+}
