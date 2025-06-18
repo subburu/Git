3db0fbc1c5bc0e08594a6e5c0cdf734cbfb2d5a1
subburusravan <subburusravan@gmail.com>
2025-06-18 14:53:37 +0530

    3 java files added

---
+++ src/com/practise/Search.java
@@ -0,0 +1,39 @@
+package com.practise;
+
+public class Search
+{
+    // Method to check if the specified element
+	// is present in the array or not
+    private static boolean isElementPresent(int[] arr, int key)
+    {
+        // check if the specified element
+        // is present in the array or not
+        // using Linear Search method
+        for (int element : arr) {
+            if (element == key) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+
+        // Get the array
+        int arr[] = { 3, 5, 7, 2, 6, 10 };
+
+        // Get the value to be checked
+        int key = 7;
+
+        // Check if this value is
+        // present in the array or not
+		boolean res = isElementPresent(arr, key);
+
+        System.out.println("Is " + key
+                           + " present in the array: "
+                           + res);
+        
+    }
+}
