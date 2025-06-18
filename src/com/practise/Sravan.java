a4ae61419369f1b3c014e667df3cc025437d421c
subburusravan <subburusravan@gmail.com>
2025-06-18 14:12:14 +0530

    added a file Sravan

---
+++ src/com/practise/Sravan.java
@@ -0,0 +1,12 @@
+package com.practise;
+
+public class Sravan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Software testing");
+
+	}
+
+}
